f56009f47dec1ce2f65ee28fa8c75bfebcbb097c
Rika-3901 <rikadoubleyou@gmail.com>
2025-07-07 17:29:58 +0700

    Update title website to LokaMarket

--- app/layout.js
+++ app/layout.js
@@ -4,7 +4,7 @@ import { ClerkProvider } from "@clerk/nextjs";
 import { Toaster } from "react-hot-toast";
 
 export const metadata = {
-  title: "QuickCart - GreatStack",
+  title: "LokaMarket - Indonesia Market",
   description: "E-Commerce with Next.js",
 };
 
