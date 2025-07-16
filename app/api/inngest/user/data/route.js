2d71d31996be33cd95346f109849badfac0dabf1
Rika-3901 <rikadoubleyou@gmail.com>
2025-07-16 15:19:56 +0700

    edit code

--- app/api/inngest/user/data/route.js
+++ app/api/inngest/user/data/route.js
@@ -1,28 +1,23 @@
-import { NextResponse } from 'next/server';
-import { auth } from '@clerk/nextjs';
-import connectDB from '@/lib/connectDB';
-import User from '../../../../../../models/user'; // ✅ Naik 4 folder ke root
+import connectDB from "@/config/db";
+import User from '@models/user';
+import { getAuth } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
 
-export async function GET() {
-        try {
-            await connectDB();
 
-            const { userId } = auth();
+export async function GET(request) {
+    try  {
+        const { userId } = getAuth(request)
 
-            if (!userId) {
-            return NextResponse.json({ success: false, message: 'Unauthorized' }, { status: 401 });
-            }
+        await connectDB()
+        const user = await User.findById(userId)
 
-            const user = await User.findById(userId);
-
-            if (!user) {
-            return NextResponse.json({ success: false, message: 'User not found' }, { status: 404 });
-            }
-
-            return NextResponse.json({ success: true, user }, { status: 200 });
-
-        } catch (error) {
-            console.error('Error fetching user data:', error);
-            return NextResponse.json({ success: false, message: 'Internal Server Error' }, { status: 500 });
+        if (!user) {
+            return NextResponse.json({ success: false, message: "User Not Found"})
         }
-        }
+
+        return NextResponse.json({succes: true, user})
+
+    } catch (error) {
+        return NextResponse.json({ success: false, message: error.message})
+    }
+}
